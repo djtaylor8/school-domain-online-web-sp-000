c32160b87dd74d7d38e8871ab3427d11546e8d5d
DJ Taylor <dtaylor88@gmail.com>
2021-06-03 16:03:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade] 
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+  
+  def grade(grade_level)
+    
+  end
+end
